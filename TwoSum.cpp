1d1735823563511cf6ee48664b7df6a729fde24c
Michael Chien <KingsDoomed.ctc@gmail.com>
2017-07-04 13:19:44 +0800

    remove comment

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,9 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 int* twoSum(int* nums, int numsSize, int target) {
 
 	int* sol = NULL;
